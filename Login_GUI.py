0b09eddf5064eeefdcc8e6ec8fc3b7f090c61719
Amirparsa-Ahmadi <86166629+Amirparsa-Ahmadi@users.noreply.github.com>
2021-07-14 16:29:55 +0430

    Add files via upload

---
+++ Login_GUI.py
@@ -0,0 +1,91 @@
+#The login app with python 
+#Librarys : sqlite3 , tkinter
+#This application maked by Amirparsa Ahmadi
+#Github : https://github.com/Amirparsa-Ahmadi
+#Aparat : https://www.aparat.com/9119zan
+#import tkinter , sqlite3
+from tkinter import *
+from tkinter import messagebox as ms
+import sqlite3
+
+with sqlite3.connect('quit.db') as db:
+    c = db.cursor()
+c.execute('CREATE TABLE IF NOT EXISTS user (username TEXT NOT NULL PRIMARY KEY,password TEX NOT NULL);')
+db.commit()
+db.close()
+
+#Class main
+class main:
+    def __init__(self,master): 
+        self.master = master
+        self.username = StringVar()
+        self.password = StringVar()
+        self.n_username = StringVar()
+        self.n_password = StringVar()
+        self.widgets()
+    #login
+    def login(self):
+        with sqlite3.connect('quit.db') as db:
+            c = db.cursor()
+
+        find_user = ('SELECT * FROM user WHERE username = ? and password = ?')
+        c.execute(find_user,[(self.username.get()),(self.password.get())])
+        result = c.fetchall()
+        if result:
+            self.logf.pack_forget()
+            self.head['text'] = self.username.get() + '\n Loged In'
+            self.head['pady'] = 150
+        else:
+            ms.showerror('Oops!','Username Not Found.')
+    #Create account        
+    def new_user(self):
+        with sqlite3.connect('quit.db') as db:
+            c = db.cursor()
+        find_user = ('SELECT username FROM user WHERE username = ?')
+        c.execute(find_user,[(self.n_username.get())])        
+        if c.fetchall():
+            ms.showerror('Error!','Username Taken Try a Diffrent One.')
+        else:
+            ms.showinfo('Success!','Account Created!')
+            self.log()
+        insert = 'INSERT INTO user(username,password) VALUES(?,?)'
+        c.execute(insert,[(self.n_username.get()),(self.n_password.get())])
+        db.commit()
+    #log
+    def log(self):
+        self.username.set('')
+        self.password.set('')
+        self.crf.pack_forget()
+        self.head['text'] = 'LOGIN'
+        self.logf.pack()
+    def cr(self):
+        self.n_username.set('')
+        self.n_password.set('')
+        self.logf.pack_forget()
+        self.head['text'] = 'Create Account'
+        self.crf.pack()
+
+    #widgets
+    def widgets(self):
+        self.logf = Frame(self.master,padx =10,pady = 10)
+        Label(self.logf,text = 'Username: ',font = ('',20),pady=5,padx=5).grid(sticky = W)
+        Entry(self.logf,textvariable = self.username,bd = 5,font = ('',15)).grid(row=0,column=1)
+        Label(self.logf,text = 'Password: ',font = ('',20),pady=5,padx=5).grid(sticky = W)
+        Entry(self.logf,textvariable = self.password,bd = 5,font = ('',15),show = '*').grid(row=1,column=1)
+        Button(self.logf,text = ' Login ',bd = 3 ,font = ('',15),padx=5,pady=5,command=self.login).grid()
+        Button(self.logf,text = ' Create Account ',bd = 3 ,font = ('',15),padx=5,pady=5,command=self.cr).grid(row=2,column=1)
+        self.logf.pack()
+        
+        self.crf = Frame(self.master,padx =10,pady = 10)
+        Label(self.crf,text = 'Username: ',font = ('',20),pady=5,padx=5).grid(sticky = W)
+        Entry(self.crf,textvariable = self.n_username,bd = 5,font = ('',15)).grid(row=0,column=1)
+        Label(self.crf,text = 'Password: ',font = ('',20),pady=5,padx=5).grid(sticky = W)
+        Entry(self.crf,textvariable = self.n_password,bd = 5,font = ('',15),show = '*').grid(row=1,column=1)
+        Button(self.crf,text = 'Create Account',bd = 3 ,font = ('',15),padx=5,pady=5,command=self.new_user).grid()
+        Button(self.crf,text = 'Go to Login',bd = 3 ,font = ('',15),padx=5,pady=5,command=self.log).grid(row=2,column=1)
+
+if __name__ == '__main__':
+    root = Tk()
+    root.title('Login')
+    main(root)
+    root.mainloop()
